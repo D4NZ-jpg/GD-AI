d02b01a5811e98a3dd328653244b77eb3862ca0a
D4NZ-jpg <57308957+D4NZ-jpg@users.noreply.github.com>
2022-10-12 19:59:30 -0500

    Replaced python server for built-in c++

--- GD Mod/dllmain.cpp
+++ GD Mod/dllmain.cpp
@@ -1,30 +1,109 @@
 // dllmain.cpp : Defines the entry point for the DLL application.
 
 #include "pch.h"
-#include "zmq.hpp"
 #include <iostream>
 #include <vector>
+#include <utility>
+#include <map>
+#include <math.h>
 #include <gd.h>
+#include <support/zip_support/ZipUtils.h>
+#include <support/base64.h>
 
-// ZeroMQ 
-zmq::context_t context(1);
-zmq::socket_t socket(context, zmq::socket_type::req);
+const int cellSize = 20;
+const int w = 20, h = 10;
+std::map<std::pair<int, int>, int> lvl_map;
+
+std::vector<std::string> split(std::string str, char delim)
+{
+    std::vector<std::string> out;
+    size_t pos = 0;
+    while ((pos = str.find(delim)) != std::string::npos)
+    {
+        out.push_back(str.substr(0, pos));
+        str.erase(0, pos + 1);
+    }
+    out.push_back(str);
+    return out;
+}
+
+std::string decode_level(const std::string& input) {
+    unsigned char* levelString;
+    unsigned char* levelStringFull;
+    int levelStringSize = base64Decode((unsigned char*)(input.c_str()), input.size(), &levelString);
+    int levelStringSizeDeflated = ZipUtils::ccInflateMemory(levelString, levelStringSize, &levelStringFull);
+
+    std::string levelStringFullStd((char*)levelStringFull, levelStringSizeDeflated);
+
+    delete levelString;
+    delete levelStringFull;
+
+    return levelStringFullStd;
+}
+
+std::map<std::pair<int,int>, int> build_level_map(std::string data)
+{
+    std::map<std::pair<int, int>, int> lvl;
+    
+    size_t pos = data.find(';');
+    data.erase(0, pos + 1);
+    for (std::string object : split(data, ';'))
+    {
+        std::vector<std::string> properties = split(object, ',');
+        if (properties.size() < 6 || properties.size() % 2)
+            continue;
+
+        int x = -1;
+        int y = -1;
+        int id = -1;
+        for (int i = 0; i < properties.size(); i += 2)
+            switch (std::stoi(properties[i]))
+            {
+            case 1:
+                id = std::stoi(properties[i+1]);
+                break;
+            case 2:
+                x = std::roundf(std::stof(properties[i + 1]) / cellSize);
+                break;
+            case 3:
+                y = std::roundf(std::stof(properties[i + 1]) / cellSize);
+                break; 
+            }
+
+        if (x != -1 && y != -1 && id != -1)
+            lvl[{x, y}] = id;
+    }
+    return lvl;
+}
+
+void visualize_map(std::pair<float,float> pos)
+{
+    int pos_x = std::roundf(pos.first / cellSize);
+    int pos_y = std::roundf(pos.second / cellSize);
+    std::string out = "";
+
+    system("cls");
+    for (int y = h; y > -h; y--)
+    {
+        for (int x = -w; x < w; x++)
+            if (lvl_map.count({ pos_x + x, pos_y + y }))
+                out += std::to_string(lvl_map[{pos_x + x, pos_y + y}]);
+            else
+                out += '.';
+        out += '\n';
+    }
+
+    out += "x: ";
+    out += std::to_string(pos_x);
+    out += ", y: ";
+    out += std::to_string(pos_y);
+    std::cout << out;
+}
 
 bool(__thiscall* PlayLayer_init)(gd::PlayLayer* self, gd::GJGameLevel* level);
 bool __fastcall PlayLayer_init_H(gd::PlayLayer* self, void*, gd::GJGameLevel* level) {
-    //Build message
-    auto msg = (std::string("init:") + level->m_sLevelString).c_str();
-
-    //Send message
-    zmq::message_t request(strlen(msg));
-    memcpy(request.data(), msg, strlen(msg));
-    socket.send(request, zmq::send_flags::none);
-    std::cout << "Sent map data\n";
-
-    //Get reply
-    zmq::message_t reply;
-    socket.recv(reply, zmq::recv_flags::none);
-
+    std::string lvl_string = decode_level(level->m_sLevelString);
+    lvl_map = build_level_map(lvl_string);
     PlayLayer_init(self, level);
     return true;
 }
@@ -32,28 +111,7 @@ bool __fastcall PlayLayer_init_H(gd::PlayLayer* self, void*, gd::GJGameLevel* le
 bool(__thiscall* PlayLayer_update)(gd::PlayLayer* self, float dt);
 bool __fastcall PlayLayer_update_H(gd::PlayLayer* self, void*, float dt)
 {
-    //Build message
-    float x = self->m_pPlayer1->getPositionX();
-    float y = self->m_pPlayer1->getPositionY();
-    auto msg = (std::string("update:") + std::to_string(x) + ',' + std::to_string(y)).c_str();
-
-    //Send message
-    zmq::message_t request(strlen(msg));
-    memcpy(request.data(), msg, strlen(msg));
-    socket.send(request, zmq::send_flags::none);
-    std::cout << "Sent update\n";
-
-    //Get reply
-    zmq::message_t reply;
-    socket.recv(reply, zmq::recv_flags::none);
-    std::cout << reply.to_string() << '\n';
-
-    if (reply.to_string() == "1")
-    {
-        self->m_pPlayer1->pushButton(0);
-        self->m_pPlayer1->releaseButton(0);
-    }
-
+    visualize_map({ self->m_pPlayer1->getPositionX(), self->m_pPlayer1->getPositionY() });
     PlayLayer_update(self, dt);
     return true;
 }
@@ -63,9 +121,6 @@ DWORD WINAPI thread(void* hModule){
     FILE* pFile = nullptr;
     AllocConsole();
     freopen_s(&pFile, "CONOUT$", "w", stdout);
-
-    // Connect ZeroMQ (Server must be already running)
-    socket.connect("tcp://localhost:6969");
 
     //Hook functions
     if (MH_Initialize() != MH_OK)
